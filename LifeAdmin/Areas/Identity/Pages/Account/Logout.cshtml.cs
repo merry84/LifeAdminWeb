baee4ca48cad540caf761e5eed9b93a7d8e2d033
Mariya Tsvetkova <144158231+merry84@users.noreply.github.com>
2026-02-14 08:23:23 +0200

    fix: identity pages use ApplicationUser (login/register/logout)

---
+++ LifeAdmin/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using LifeAdminModels.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LifeAdmin.Web.Areas.Identity.Pages.Account
+{
+    public class LogoutModel : PageModel
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public LogoutModel(SignInManager<ApplicationUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public async Task<IActionResult> OnPost(string? returnUrl = null)
+        {
+            await _signInManager.SignOutAsync();
+
+            if (returnUrl != null)
+                return LocalRedirect(returnUrl);
+
+            return RedirectToPage();
+        }
+    }
+}
--- LifeAdminData/ApplicationDbContext.cs
+++ LifeAdminData/ApplicationDbContext.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 namespace LifeAdminData
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
