ea4e5dffc7ef1b9383132f184724b4871ca64678
Mariya Tsvetkova <144158231+merry84@users.noreply.github.com>
2026-04-04 18:11:11 +0300

    UI: Add About page with modern design and improve visual consistency across pages

--- LifeAdmin/Controllers/HomeController.cs
+++ LifeAdmin/Controllers/HomeController.cs
@@ -41,6 +41,10 @@ namespace LifeAdmin.Web.Controllers
         {
             return View();
         }
+        public IActionResult About()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
