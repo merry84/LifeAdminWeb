605aa9ebaad4735a7ae03f43c2d67637ff58dd4f
Mariya Tsvetkova <144158231+merry84@users.noreply.github.com>
2026-04-07 00:09:17 +0300

    Add Identity Manage pages (ChangePassword, SetPassword) and UI improvements

---
+++ LifeAdmin/Areas/Identity/Pages/Account/Manage/SetPassword.cshtml.cs
@@ -0,0 +1,64 @@
+﻿using LifeAdminModels.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace LifeAdmin.Web.Areas.Identity.Pages.Account.Manage
+{
+    [Authorize]
+    public class SetPasswordModel : PageModel
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public SetPasswordModel(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [DataType(DataType.Password)]
+            public string NewPassword { get; set; }
+
+            [DataType(DataType.Password)]
+            [Compare("NewPassword")]
+            public string ConfirmPassword { get; set; }
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+                return Page();
+
+            var user = await _userManager.GetUserAsync(User);
+
+            var result = await _userManager.AddPasswordAsync(user, Input.NewPassword);
+
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                    ModelState.AddModelError(string.Empty, error.Description);
+
+                return Page();
+            }
+
+            await _signInManager.RefreshSignInAsync(user);
+            return RedirectToPage("./Index");
+        }
+    }
+}
